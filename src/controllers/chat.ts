332280b5b74a2c7afa4f3caccf900ef24662b28c
vikash <44chansw@gmail.com>
2022-07-21 15:54:54 +0530

    msg send

--- src/controllers/chat.ts
+++ src/controllers/chat.ts
@@ -7,9 +7,13 @@ class Connection {
     constructor(io: any, socket: Socket) {
         this.socket = socket
         this.io = io
-
         socket.on('connect_error', (err: Error) => {
             console.log(`connect_error due to ${err.message}`)
+        })
+
+        socket.on('msg', (data) => {
+            console.log(socket.handshake.auth)
+            console.log(data)
         })
     }
 
